673f86c4e80689b5aa1e07ab9c8bc1dae7b5351e
Leekiahn <68183471+Leekiahn@users.noreply.github.com>
2025-10-24 12:43:04 +0900

    refactor: 프리팹/직접 생성 방식 혼용 부분 수정

--- Assets/Scripts/FactoryMethod/SkeletonFactory.cs
+++ Assets/Scripts/FactoryMethod/SkeletonFactory.cs
@@ -4,12 +4,7 @@ namespace FactoryMethod
 {
     public class SkeletonFactory : GenericMonsterFactory<SkeletonMonster>
     {
-        public override BaseMonster SpawnMonster()
-        {
-            GameObject skeletonObject = new GameObject("Skeleton");
-            SkeletonMonster skeleton = skeletonObject.AddComponent<SkeletonMonster>();
-            return skeleton;
-        }
+        
     }
 }
 
